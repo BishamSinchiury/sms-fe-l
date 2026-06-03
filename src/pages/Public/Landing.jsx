42cb856465f4d3d3dbffcdeb9f638b503c89fe57
Bisham Sinchiury <bishamsinchiury1116@gmail.com>
2026-06-03 11:05:12 +0545

    changes

--- src/pages/Public/Landing.jsx
+++ src/pages/Public/Landing.jsx
@@ -4,12 +4,10 @@ import Button from '../../components/Botton/Button'
 const Landing = () => {
   return (
     <>
-    <Button type="Save">
-      Login
-    </Button>
-    <Button>
-      Logout
-    </Button>
+      <h1>Company Name</h1>
+      <h3>Company Adrress</h3>
+      <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae fugit optio excepturi architecto cum est quae sapiente dolorem culpa. Neque, deserunt. Sunt deserunt totam reprehenderit excepturi repellat, adipisci possimus repellendus?</h6>
+      <Button>Learn More</Button>
     </>
   )
 }
